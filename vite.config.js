363d08ec5aa39330680c321c25a8be63bd0803e1
Athena <az2679@nyu.edu>
2024-04-19 20:26:58 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import glsl from 'vite-plugin-glsl';
@@ -7,4 +8,15 @@ export default defineConfig({
     port: 3333,
   },
   plugins: [react(), glsl()],
+  // root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
+  // base: '/tangible-values/',
+  build: {
+    outDir: '../docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
